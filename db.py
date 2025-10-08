e10de69cae0126883014c7f1e22cc5c5c4d3509b
BZZZ85 <55660216+BZZZ85@users.noreply.github.com>
2025-10-08 22:03:02 +0300

    db.py

--- db.py
+++ db.py
@@ -33,12 +33,19 @@ async def close_pool():
 
 
 # ===== Пользователи =====
-async def add_user(user_id: int, username: str):
-    async with pool.acquire() as conn:
-        await conn.execute(
-            "INSERT INTO users (user_id, username) VALUES ($1, $2) ON CONFLICT (user_id) DO NOTHING",
-            user_id, username
-        )
+import asyncpg
+
+async def add_user(pool, user_id, username):
+    try:
+        async with pool.acquire() as conn:
+            await conn.execute("""
+                INSERT INTO users (user_id, username)
+                VALUES ($1, $2)
+                ON CONFLICT (user_id) DO NOTHING;
+            """, user_id, username)
+    except Exception as e:
+        print(f"DB error add_user: {e}")
+
 
 # ===== Упражнения =====
 async def get_exercises(user_id: int):
