debd045dce3204867f119344496155d9bba31f01
BZZZ85 <55660216+BZZZ85@users.noreply.github.com>
2025-10-10 13:10:24 +0300

    bot_mvp.py

--- bot_mvp.py
+++ bot_mvp.py
@@ -1,7 +1,7 @@
 import os
 import asyncio
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import CommandStart, F
+from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
