7230acaeba95363811e38ba4be8954b5fcef919c
BZZZ85 <55660216+BZZZ85@users.noreply.github.com>
2025-10-13 19:08:12 +0300

    bot_mvp.py

--- bot_mvp.py
+++ bot_mvp.py
@@ -21,7 +21,7 @@ if os.path.exists("ton.env"):
 
 # Получаем переменные окружения (Railway Variables или ton.env)
 print("DEBUG: available env keys:", sorted(k for k in os.environ.keys() if "BOT" in k or "TOKEN" in k))
-print("DATABASE_URL =", DATABASE_URL)
+
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
