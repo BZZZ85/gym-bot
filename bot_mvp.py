0e99c1ddd83e4f33b007e7a99213cdce6c74c938
BZZZ85 <55660216+BZZZ85@users.noreply.github.com>
2025-10-14 17:03:36 +0300

    bot_mvp.py

--- bot_mvp.py
+++ bot_mvp.py
@@ -16,8 +16,7 @@ from datetime import datetime
 from aiogram import types
 from aiogram.types import FSInputFile
 from datetime import datetime, timedelta, time
-
- import pytz
+import pytz
 # Загружаем локальный .env только если он есть
 if os.path.exists("ton.env"):
     load_dotenv("ton.env")
